f42be83fba12dc44a61cd8c2f069f0e4389f4bf4
Azir@9200 <aziruddin83@hotmail.com>
2025-03-05 04:25:14 +0000

    file uploader added

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+import { Box, Stack, Button, TextField } from "@mui/material";
+import SpecialtyModal from "./components/SpecialtyModal";
+import { useState } from "react";
+
 const SpecialtiesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
-    <div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button onClick={() => setIsModalOpen(true)}>Create Specialty</Button>
+        <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Specialist" />
+      </Stack>
       <h2>Here is all specialties page</h2>
-    </div>
+    </Box>
   );
 };
 
