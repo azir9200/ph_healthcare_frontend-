4ed1c619a7c6aeaec72c22ade2ed388f6da743bb
Azir@9200 <aziruddin83@hotmail.com>
2025-07-21 21:34:54 +0100

    speciality fouund by id

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -20,6 +20,7 @@ const Specialist = () => {
         const res = await fetch("http://localhost:5000/api/v1/specialties", {
           next: { revalidate: 30 },
         });
+        console.log("object res", res);
         if (!res.ok) {
           throw new Error(`HTTP error! Status: ${res.status}`);
         }
